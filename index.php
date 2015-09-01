ccad2ea737f40337f166951e809a2bdbfa9c70f8
Mike Swan <mike@digimute.com>
2015-09-01 11:05:20 +0100

    new pages

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php 
 
-echo "start";
+// Main engine defines    
+define('DS', DIRECTORY_SEPARATOR);
+
+// enable error temporarily in case there are startup errors 
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(-1);
+
+require_once( 'lib' . DS . 'core.php');
+
+$system = new Core();
+
---
+++ lib/core.php
@@ -0,0 +1,75 @@
+<?php 
+
+class Core {
+
+  public $pages = array();
+  public $output = ''; 
+  
+	public function __construct() {
+	
+	  $this->pages = self::dirToArray(self::getRootPath().'ogma-docs\pages');
+    $this->getMenu();
+    echo $this->output;
+	}
+
+
+  public function getMenu(){
+    $pages = $this->pages;
+    $this->output .= '<ul class="topics">';
+    foreach($pages as $key=>$subpages){
+      $parts = explode('-', $key);
+      $this->output .= '<li class="dd-item visited" data-nav-id="/">
+            <a href="/'.strtolower($parts[1]).'">
+                <i class="fa fa-check read-icon"></i>
+                <span><b>'.$parts[1].'</span>
+            </a>';
+        if (is_array($subpages)){
+          $this->output .= '<ul>';
+          foreach($subpages as $page){
+            $parts = explode('-', $page);
+            $this->output .= '<li>'.$parts[1].'</li>';
+          }
+          $this->output .= '</ul>';
+        }
+        $this->output .= '</li>';
+    } 
+    $this->output .= '</ul>';
+  }
+
+	public static function dirToArray($dir) { 
+   	$result = array(); 
+    $cdir = scandir($dir); 
+    foreach ($cdir as $key => $value) 
+    { 
+      if (!in_array($value,array(".",".."))) 
+      { 
+        if (is_dir($dir . DIRECTORY_SEPARATOR . $value)) 
+        { 
+            $result[$value] = self::dirToArray($dir . DIRECTORY_SEPARATOR . $value); 
+        } 
+        else 
+        { 
+            $result[] = $value; 
+        } 
+      } 
+    } 
+    return $result; 
+    } 
+    
+    
+	public static function getRootPath() {
+		$pos = strrpos(dirname(__FILE__),DIRECTORY_SEPARATOR.'lib');
+		$adm = substr(dirname(__FILE__), 0, $pos);
+		$pos2 = strrpos($adm,DIRECTORY_SEPARATOR);
+		return self::tsl(substr(__FILE__, 0, $pos2));
+	}	
+
+
+	public static function tsl($path) {
+		if( substr($path, strlen($path) - 1) != '/' ) {
+			$path .= DS;
+		}
+		return $path;
+	}
+  
+}
